38bc6fc8f8c80020eacf5899fff98c32c7ae947b
yu songyi <songyi0207@gmail.com>
2019-06-30 19:02:33 +0900

    Set channel in flutter

--- lib/headset_event.dart
+++ lib/headset_event.dart
@@ -2,12 +2,32 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+typedef DetectPluggedCallback = Future<dynamic> Function(String payload);
+
 class HeadsetEvent {
   static const MethodChannel _channel =
-      const MethodChannel('headset_event');
+      const MethodChannel('flutter.moum/headset_event');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static initDetectHandler() {
+    print('initDetectHandler');
+    _channel.setMethodCallHandler(_handleMethod);
+  }
+
+  static Future<dynamic> _handleMethod(MethodCall call) async {
+    switch(call.method) {
+      case "plugged":
+        print('headset plugged!!');
+        return new Future.value(true);
+      case "unplugged":
+        print('headset unplugged!!');
+        return new Future.value(false);
+    }
+  }
+
+
 }
