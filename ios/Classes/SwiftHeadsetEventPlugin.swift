332769ae773af60d0b4b6ea5b394c420aa1e2fcc
yu songyi <songyi0207@gmail.com>
2019-06-30 19:30:14 +0900

    ios change channel name

--- ios/Classes/SwiftHeadsetEventPlugin.swift
+++ ios/Classes/SwiftHeadsetEventPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftHeadsetEventPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "headset_event", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "flutter.moum/headset_event", binaryMessenger: registrar.messenger())
     let instance = SwiftHeadsetEventPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
