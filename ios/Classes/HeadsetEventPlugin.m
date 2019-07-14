7abbd6a1888117cfec090ece7c100304c53b9e5f
yu songyi <songyi0207@gmail.com>
2019-07-14 20:16:43 +0900

    swift to object c

--- ios/Classes/HeadsetEventPlugin.m
+++ ios/Classes/HeadsetEventPlugin.m
@@ -1,8 +1,20 @@
 #import "HeadsetEventPlugin.h"
-#import <headset_event/headset_event-Swift.h>
 
 @implementation HeadsetEventPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftHeadsetEventPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"headset_event"
+            binaryMessenger:[registrar messenger]];
+  HeadsetEventPlugin* instance = [[HeadsetEventPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
