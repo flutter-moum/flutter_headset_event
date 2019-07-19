80d25c5537484b71b9209b756fe204370e556b29
yu songyi <songyi0207@gmail.com>
2019-07-19 22:47:25 +0900

    Revert "swift to object c"
    This reverts commit 7abbd6a1888117cfec090ece7c100304c53b9e5f.

--- ios/Classes/HeadsetEventPlugin.m
+++ ios/Classes/HeadsetEventPlugin.m
@@ -1,20 +1,8 @@
 #import "HeadsetEventPlugin.h"
+#import <headset_event/headset_event-Swift.h>
 
 @implementation HeadsetEventPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"headset_event"
-            binaryMessenger:[registrar messenger]];
-  HeadsetEventPlugin* instance = [[HeadsetEventPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftHeadsetEventPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
